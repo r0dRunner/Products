d11f9240a8fc447360144445051e0dd1598524c2
Rodney Cabahug <rodney.cabahug@xero.com>
2017-10-23 17:26:56 +1300

    Added created scripts for tables.

---
+++ Products.Database/Table.ProductOption.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ProductOption] (
+    [Id]          UNIQUEIDENTIFIER CONSTRAINT [DF_ProductOptionId] DEFAULT (newsequentialid()) NOT NULL,
+    [ProductId]   UNIQUEIDENTIFIER NOT NULL,
+    [Name]        NVARCHAR (100)   NOT NULL,
+    [Description] NVARCHAR (500)   NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_ProductOption_Product] FOREIGN KEY ([ProductId]) REFERENCES [dbo].[Product] ([Id]) ON DELETE CASCADE
+);
